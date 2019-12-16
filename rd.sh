2189ba72d34b184157ccf90b6eb5d57c5333e11a
root <root@HS-TECH-MH.PASD.WAN>
2019-12-16 15:04:09 -0800

    Added help basic message, as well as the ability to roll multiple sets of dice at once

--- rd.sh
+++ rd.sh
@@ -10,15 +10,13 @@
 function rd
 {
     # Check parameters
-    if [ $# -lt 1 ]
-    then
-        echo "Please provide dice information"
+    if [ $# -lt 1 ]; then
+        echo "Please provide dice information."
         return 1
     fi 
 
-    if [ $# -gt 1 ]
-    then
-        echo "Too many inputs"
+    if [ $# -gt 2 ]; then
+        echo "Too many inputs."
         return 1
     fi 
 
@@ -27,10 +25,19 @@ function rd
     DICEINFO=$1
 
 
+    # print help information
+    if [[ $DICEINFO == "-h" ]] ||	# accept multiple help formats
+       [[ "$(echo $DICEINFO | tr '[:upper:]' '[:lower:]')" == "h" ]] ||
+       [[ "$(echo $DICEINFO | tr '[:upper:]' '[:lower:]')" == "help" ]]; then
+       	echo "Format for one set of dice: rd 4d6"
+       	echo "Format for multiple sets of dice: rd 4d6 6"
+       	return 0;
+    fi
+
+
     # check the input to ensure it is of proper format: [uint]d[unit]
-    if ! [[ $DICEINFO =~ ^[1-9][0-9]*[d][1-9][0-9]*$ ]]
-    then
-        echo "Incorrect dice format. Format should follow standard DnD notation: 2d6, 5d4, etc"
+    if ! [[ $DICEINFO =~ ^[1-9][0-9]*[d][1-9][0-9]*$ ]]; then
+        echo "Incorrect dice format. Format should follow standard DnD notation: 2d6, 5d4, etc."
         return 2
     fi
 
@@ -42,19 +49,35 @@ function rd
     IFS='d'
     read -ra INFO <<< "$DICEINFO"
 
-    DQTY="${INFO[0]}"
-    SIDES="${INFO[1]}"
+    ROLLS=0				# number of times the dice are rolled
+    DQTY="${INFO[0]}"	# number of dice to roll
+    SIDES="${INFO[1]}"	# number of sides per die
 
+    # if only one input, roll dice once
+    if [ $# -lt 1 ]; then
+        ROLLS=1
+    elif [ $# -eq 2 ]; then
+    	# check the format of $2
+    	if ! [[ $2 =~ ^[1-9][0-9]*$ ]]; then
+    		echo "Incorrect amount of dice rolls"
+    		return 3
+    	fi
 
-    # roll for each die
-    for ((i=1;i<=$DQTY;i++))
-    do
-        # roll the die
-        ROLL=$((1 + RANDOM % $SIDES))
+    	# if the format is correct, set ROLLS to $2
+    	ROLLS=$2
+    fi
 
-        # print to console (-n prevents printing newline)
-        echo -n "$ROLL "
-    done; echo
+    # roll all dice $ROLLS amount of times
+    for ((i=1;i<=$ROLLS;i++)); do
+	    # roll each die
+	    for ((j=1;j<=$DQTY;j++)); do
+	        # roll the die
+	        ROLL=$((1 + RANDOM % $SIDES))
+
+	        # print to console (-n prevents printing newline)
+	        echo -n "$ROLL "
+	    done; echo
+	done;
 
     # restore the original IFS
     IFS=$OIFS
